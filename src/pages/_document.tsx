2a6006ce03ba02357c6c04a6b028a04d0ee1bb89
jizik <jizik13@gmail.com>
2023-02-26 17:49:27 +0300

    feat: make app to test playing streams

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column',
+        }}
+      >
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
